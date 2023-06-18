40113e86b0ac1a5ef491c69f13558f96f5c79851
howdilla <114222361+howdilla@users.noreply.github.com>
2023-06-18 11:54:59 +0300

    Create officeAdminController.java

---
+++ officeAdminController.java
@@ -0,0 +1,128 @@
+package com.example.iq02;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class officeAdminController {
+
+    @FXML
+    private TextField dr_account_Admin;
+
+    @FXML
+    private Button editPass_Admin_button;
+
+    @FXML
+    private Button editProfile_Admin_button;
+
+    @FXML
+    private TextField email_account_Admin;
+
+    @FXML
+    private TableColumn<?, ?> finish_for_Admin;
+
+    @FXML
+    private TextField group_account_Admin;
+
+    @FXML
+    private Tab lc_Admin1;
+
+    @FXML
+    private Tab lc_Admin2;
+
+    @FXML
+    private Tab lc_Admin3;
+
+    @FXML
+    private TableColumn<?, ?> log_for_Admin;
+
+    @FXML
+    private TextField login_account_Admin;
+
+    @FXML
+    private Text nameAdmin;
+
+    @FXML
+    private Text nameAdmin1;
+
+    @FXML
+    private Text nameAdmin11;
+
+    @FXML
+    private Text nameAdmin111;
+
+    @FXML
+    private Text nameAdmin112;
+
+    @FXML
+    private TextField name_account_Admin;
+
+    @FXML
+    private TextField phone_account_Admin;
+
+    @FXML
+    private TableView<?> table_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_answer_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_birthday_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_email_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_group_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_login_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_name_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_password_Admin;
+
+    @FXML
+    private TableColumn<?, ?> table_phonenumber_Admin;
+
+    @FXML
+    private TableColumn<?, ?> test1_for_Admin;
+
+    @FXML
+    private TableColumn<?, ?> test2_for_Admin;
+
+    @FXML
+    private TableColumn<?, ?> test3_for_Admin;
+
+    @FXML
+    private TableColumn<?, ?> test4_for_Admin;
+
+    @FXML
+    private TableColumn<?, ?> test5_for_Admin;
+
+    @FXML
+    private Button testingAccount_Admin_button;
+
+    @FXML
+    void goEditOffice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goEditPassw(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goTestinge(ActionEvent event) {
+
+    }
+
+}
